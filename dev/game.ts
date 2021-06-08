327779d8680f38279b74ff6e4529154f8929e820
Nils <vosnils18@icloud.com>
2021-06-08 22:14:30 +0200

    Goed begin is het halve werk.

--- dev/game.ts
+++ dev/game.ts
@@ -1,11 +1,26 @@
+import { PlayerCharacter } from "./playercharacter.js"
+import { UI } from "./ui.js"
+
 class Game {
+    private playerCharacter : PlayerCharacter
+    private ui : UI
+    paused : boolean
 
     constructor(){
-        this.gameLoop()
+        this.ui = new UI()
+        this.playerCharacter = new PlayerCharacter()
+        this.paused = false
+
+        this.playerCharacter.update()
+        this.ui.update()
     }
 
     gameLoop() {
-
+        this.ui.update()
+        this.playerCharacter.update()
+        if ( !this.paused ) {
+            requestAnimationFrame(() => this.gameLoop())
+        }
     }
 }
 
---
+++ dev/playercharacter.ts
@@ -0,0 +1,59 @@
+export class PlayerCharacter {
+    private element : HTMLElement
+    private xspeed : number = 0
+    private yspeed : number = 0
+    private x : number = 40
+    private y : number = 120
+
+    constructor(){
+        window.addEventListener("keydown", (e:KeyboardEvent) => this.onKeyDown(e))
+        window.addEventListener("keyup", (e:KeyboardEvent) => this.onKeyUp(e))
+        const game = document.querySelector('game') as HTMLElement
+        this.element = document.createElement("playercharacter")
+        game.appendChild(this.element)
+    }
+
+    public update(): void{
+        this.element.style.transform = `translate(${this.x}px, ${this.y}px)`
+        this.x += this.xspeed
+        this.y += this.yspeed
+    }
+
+    private onKeyDown(e: KeyboardEvent): void {
+        switch (e.key.toUpperCase()) {
+            case "A":
+            case "ARROWLEFT" :
+                this.xspeed = -5
+                break
+            case "D":
+            case "ARROWRIGHT":
+                this.xspeed = 5
+                break
+            case "W":
+            case "ARROWUP" :
+                this.yspeed = -5
+                break
+            case "S":
+            case "ARROWDOWN":
+                this.yspeed = 5
+                break
+        }
+    }
+
+    private onKeyUp(e: KeyboardEvent): void {
+        switch (e.key.toUpperCase()) {
+            case "A":
+            case "D":
+            case "ARROWLEFT" :
+            case "ARROWRIGHT":
+                this.xspeed = 0
+                break
+            case "W":
+            case "S":
+            case "ARROWUP":
+            case "ARROWDOWN":
+                this.yspeed = 0
+                break
+        }
+    }
+}
